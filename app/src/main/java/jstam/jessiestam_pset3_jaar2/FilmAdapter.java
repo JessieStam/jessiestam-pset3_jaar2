40e7492f2ebe271e2e06bbc6bc501434f390b7e6
JessieStam <jessiestam@quicknet.nl>
2016-09-26 10:04:01 +0200

    HttpRequestHelper doesn't recognize the params (title string) I'm giving it. Help.

--- app/src/main/java/jstam/jessiestam_pset3_jaar2/FilmAdapter.java
+++ app/src/main/java/jstam/jessiestam_pset3_jaar2/FilmAdapter.java
@@ -1,17 +1,18 @@
 package jstam.jessiestam_pset3_jaar2;
 
 import android.graphics.Color;
-import android.net.sip.SipAudioCall;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by Jessie on 22/09/2016.
  */
 
-public class FilmAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+public class FilmAdapter extends RecyclerView.Adapter<FilmAdapter.ViewHolder> {
 
     //krijgt data mee (title, poster)
 
@@ -20,11 +21,12 @@ public class FilmAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     private String finished = "Gray";
 
     View.OnClickListener listener = new View.OnClickListener() {
+
         @Override
         public void onClick(View view) {
 
             // if item is selected, change color to gray
-            if (currentColor.equals(unfinished) || currentColor.equals(null)) {
+            if (currentColor.equals(unfinished) || currentColor == null) {
                 view.setBackgroundColor(Color.GRAY);
                 currentColor = finished;
             }
@@ -33,29 +35,44 @@ public class FilmAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
                 view.setBackgroundColor(Color.WHITE);
                 currentColor = unfinished;
             }
+
         }
     }
 
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private int[] posters;
+        private String[] titles;
+
+        public TextView myTextView;  //voor namen
+        ImageView myImageView; //voor plaatjes
+
+        public ViewHolder(View itemView) { //krijgt mee wat je erin wilt stoppen
+            super(itemView);
+            myTextView = (TextView) itemView.findViewById(R.id.titles);
+            myImageView = (ImageView) itemView.findViewById(R.id.posters);
+
+        }
+    }
 
     @Override
     public FilmAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int position) {
 
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_main, parent, false);
-        ViewHolder vH = new RecyclerView.ViewHolder(view);
+        ViewHolder viewHolder = new ViewHolder(view);
         return viewHolder;
-
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(FilmAdapter.ViewHolder viewHolder, int position) {
 
+        viewHolder.myTextView.setText(titles[i]);
+        viewHolder.myImageView.setImageResource(posters[i]);
+        viewHolder.itemView.setOnClickListener(listener);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return titles.length();
     }
 
-    private class ViewHolder {
-    }
 }
